06e77310ed1342fed6f80939b8e4b5cb3a646561
60254 <60254@ITS00238.mjsystem.com>
2021-06-21 13:16:24 +0900

    豆

---
+++ src/bean/kanrishaBean.java
@@ -0,0 +1,30 @@
+package bean;
+
+public class kanrishaBean {
+
+	private String k_id;
+	private String k_name;
+	private String k_pass;
+
+	public String getId() {
+		return k_id;
+	}
+	public void setId(String k_id) {
+		this.k_id = k_id;
+	}
+
+	public String getName() {
+		return k_name;
+	}
+	public void setName(String k_name) {
+		this.k_name = k_name;
+	}
+
+	public String getPass() {
+		return k_pass;
+	}
+	public void setPass(String k_pass) {
+		this.k_pass = k_pass;
+	}
+
+}
