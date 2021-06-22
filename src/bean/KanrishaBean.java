aa8ce8fc8fd558efd2bda923fb3b6f8c62766201
60254 <60254@ITS00238.mjsystem.com>
2021-06-22 14:31:41 +0900

    bean消えた

---
+++ src/bean/KanrishaBean.java
@@ -0,0 +1,39 @@
+package bean;
+
+public class KanrishaBean {
+
+	private String kId;
+	private String kName;
+	private String postId;
+	private String kPass;
+
+	public String getId() {
+		return kId;
+	}
+	public void setId(String kId) {
+		this.kId = kId;
+	}
+
+	public String getName() {
+		return kName;
+	}
+	public void setName(String kName) {
+		this.kName = kName;
+	}
+
+	public String getPostId() {
+		return postId;
+	}
+	public void setPostId(String postId) {
+		this.postId = postId;
+	}
+
+	public String getPass() {
+		return kPass;
+	}
+	public void setPass(String kPass) {
+		this.kPass = kPass;
+	}
+
+}
+
