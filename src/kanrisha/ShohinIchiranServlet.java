4eda6ced109b49c022e605bf395f76a49cb0c9df
60255 <60255@ITS00239.mjsystem.com>
2021-06-22 13:59:36 +0900

    商品一覧サーブレット

---
+++ src/kanrisha/ShohinIchiranServlet.java
@@ -0,0 +1,35 @@
+package kanrisha;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShohinIchiranServlet
+ */
+@WebServlet("/ShohinIchiranServlet")
+public class ShohinIchiranServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//文字コードを設定する
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+
+
+	}
+
+}
