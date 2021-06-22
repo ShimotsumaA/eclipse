c351846df954d994417d0748a8b8164182911a74
60263 <60263@ITS00247.mjsystem.com>
2021-06-22 13:50:41 +0900

    変更

---
+++ src/shohisha/ShohinDao.java
@@ -0,0 +1,163 @@
+package shohisha;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import bean.shohinBean;
+import sogo.DBAccess;
+
+public class ShohinDao extends DBAccess {
+
+	//商品テーブル全件検索
+	public ArrayList<shohinBean> selectAll(){
+		ArrayList<shohinBean>list=new ArrayList<shohinBean>();
+
+		String sql="SELECT * FROM shohin";
+
+		try {
+			connect();
+			PreparedStatement ps=getConnection().prepareStatement(sql);
+			ResultSet rs=ps.executeQuery();
+
+			while(rs.next()) {
+				shohinBean bean = new shohinBean();
+
+				bean.setShohinId(rs.getString("shohinId"));
+				bean.setShohinName(rs.getString("shohinName"));
+				bean.setKijiId(rs.getString("kijiId"));
+				bean.setCategoryId(rs.getString("categoryId"));
+				bean.setValue(rs.getBigDecimal("value"));
+				list.add(bean);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+
+		}finally {
+			disconnect();
+		}
+		return list;
+	}
+
+
+	//商品IDをもとに商品テーブルを検索
+	public ArrayList<shohinBean> joken(String shohinId) {
+		shohinBean bean =new shohinBean();
+		ArrayList<shohinBean>list=new ArrayList<shohinBean>();
+
+		String sql="SELECT shohinId, shohinName, kijiId, categoryId,value FROM shohin where shohinId=?";
+
+		try {
+			connect();
+			PreparedStatement ps=getConnection().prepareStatement(sql);
+			ps.setString(1, shohinId);
+			ResultSet rs=ps.executeQuery();
+
+			while(rs.next()) {
+				bean.setShohinId(rs.getString("shohinId"));
+				bean.setShohinName(rs.getString("shohinName"));
+				bean.setKijiId(rs.getString("kijiId"));
+				bean.setCategoryId(rs.getString("categoryId"));
+				bean.setValue(rs.getBigDecimal("value"));
+				list.add(bean);
+			}
+
+
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			disconnect();
+		}
+		return list;
+
+		}
+
+
+	//新規登録メソッド
+		public int insert(String shohinId,String shohinName, String kijiId, String categoryId,BigDecimal value){
+
+			String sql = "INSERT into shohisha(shohinId,shohinName,kijiId,categoryId,value)"
+					+ "values(?,?,?,?,?)";
+
+			int rs=0;
+
+			try {
+				connect();
+
+				PreparedStatement ps=getConnection().prepareStatement(sql);
+				ps.setString(1, shohinId);
+				ps.setString(2, shohinName);
+				ps.setString(3, kijiId);
+				ps.setString(4, categoryId);
+				ps.setBigDecimal(5,value);
+
+
+				rs=ps.executeUpdate();
+
+
+
+	    	}catch(Exception e) {
+	    		e.printStackTrace();
+
+	    	}finally {
+	    		disconnect();
+	    	}
+			return rs;
+		}
+
+	//商品IDをもとに情報更新メソッド
+		public int update(String shohinId,String shohinName, String kijiId, String categoryId,BigDecimal value) {
+
+				String sql = "UPDATE shohin SET shohinName=?, kijiId=?,categoryId=?,value=? where shohinId=?";
+
+				int rs=0;
+
+				try {
+					connect();
+					PreparedStatement ps=getConnection().prepareStatement(sql);
+
+					ps.setString(1, shohinName);
+					ps.setString(2, kijiId);
+					ps.setString(3, categoryId);
+					ps.setBigDecimal(4, value);
+					ps.setString(5,shohinId);
+
+
+					rs=ps.executeUpdate();
+
+
+
+		    	}catch(Exception e) {
+		    		e.printStackTrace();
+
+		    	}finally {
+		    		disconnect();
+		    	}
+				return rs;
+		}
+
+	//削除メソッド
+		public int delete(String shohinId) {
+
+			String sql="DELETE FROM shohin where shohinId=?";
+			int rs=0;
+
+			try {
+				connect();
+				PreparedStatement ps=getConnection().prepareStatement(sql);
+
+				ps.setString(1, shohinId);
+
+				rs=ps.executeUpdate();
+
+			}catch(Exception e) {
+	    		e.printStackTrace();
+
+	    	}finally {
+	    		disconnect();
+	    	}
+			return rs;
+		}
+}
