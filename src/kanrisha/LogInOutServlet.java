329a6afa90ed83ae172e4106646fdf26546b5cb6
60254 <60254@ITS00238.mjsystem.com>
2021-06-22 11:43:19 +0900

    ログインサーブレット

--- src/kanrisha/LogInOutServlet.java
+++ src/kanrisha/LogInOutServlet.java
@@ -2,41 +2,28 @@ package kanrisha;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class logInOutServlet
- */
-@WebServlet("/logInOutServlet")
+@WebServlet("/LogInOutServlet")
 public class LogInOutServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogInOutServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		//文字コードを設定する
+				request.setCharacterEncoding("UTF-8");
+				response.setContentType("text/html;charset=UTF-8");
+
+		//管理者総合メニューへ遷移する
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/group2work/menu.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
