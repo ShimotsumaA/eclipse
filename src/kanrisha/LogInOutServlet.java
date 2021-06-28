c0a803418637527f322843447f919f51ad879bc5
60261 <60261@ITS00245.mjsystem.com>
2021-06-28 13:06:49 +0900

    変更

--- src/kanrisha/LogInOutServlet.java
+++ src/kanrisha/LogInOutServlet.java
@@ -15,27 +15,29 @@ import sogo.ErrCheck;
 @WebServlet("/LogInOutServlet")
 public class LogInOutServlet extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doPost(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		//文字コードを設定する
+		// 文字コードを設定する
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
 
-		//セッション領域を作成する
+		// セッション領域を作成する
 		HttpSession session = request.getSession(true);
 
-		//submitの値によってログインかログアウト
+		// submitの値によってログインかログアウト
 		String submit = request.getParameter("submit");
 		System.out.println(submit);
 
-		//ログインする
+		// ログインする
 		if (submit.equals("ログイン")) {
 
-			//管理者かお客様か。
+			// 管理者かお客様か。
 			String zokusei = request.getParameter("zokusei");
 			System.out.println(zokusei);
 
@@ -44,12 +46,12 @@ public class LogInOutServlet extends HttpServlet {
 			String id = request.getParameter("id");
 			String pass = request.getParameter("pass");
 
-			System.out.println(id+pass);
+			System.out.println(id + pass);
 
-			//管理者エラーチェック
-			if (zokusei.equals("shain") || zokusei.equals("aru") ) {
+			// 管理者エラーチェック
+			if (zokusei.equals("shain") || zokusei.equals("aru")) {
 
-				//管理者IDが存在するか。
+				// 管理者IDが存在するか。
 				if (err.existkId(id)) {
 					System.out.println("存在");
 				} else {
@@ -60,35 +62,31 @@ public class LogInOutServlet extends HttpServlet {
 					dispatcher.forward(request, response);
 				}
 
-
-				//管理者IDとパスワードが一致するか。
+				// 管理者IDとパスワードが一致するか。
 				if (err.kPassCollect(id, pass)) {
 					System.out.println("一致");
 				} else {
 					String message = "IDとパスワードが一致しません。";
 					request.setAttribute("message", message);
 
-				    RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/sogo/login.jsp?no=3");
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/sogo/login.jsp?no=3");
 					dispatcher.forward(request, response);
 				}
 
+				// IDパスワードが正しいので、IDをセッション領域に預ける
+				session.setAttribute("id", id);
 
-				//IDパスワードが正しいので、IDをセッション領域に預ける
-				session.setAttribute("id",id);
-
-				//社員、アルバイトの属性をセッション領域に預ける
+				// 社員、アルバイトの属性をセッション領域に預ける
 				session.setAttribute("zokusei", zokusei);
 
-
-				//管理者総合メニューへ遷移する
+				// 管理者総合メニューへ遷移する
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/sogo/kanrisha/menu.jsp");
 				dispatcher.forward(request, response);
 
-
-			//消費者（お客様）エラーチェック
+				// 消費者（お客様）エラーチェック
 			} else {
 
-				//消費者IDが存在するか。
+				// 消費者IDが存在するか。
 				if (err.existSId(id)) {
 
 				} else {
@@ -99,39 +97,42 @@ public class LogInOutServlet extends HttpServlet {
 					dispatcher.forward(request, response);
 				}
 
-				//消費者IDとパスワードが一致するか。
+				// 消費者IDとパスワードが一致するか。
 				if (err.sPassCollect(id, pass)) {
 
-			} else {
-				String message = "IDとパスワードが一致しません。";
-				request.setAttribute("message", message);
+				} else {
+					String message = "IDとパスワードが一致しません。";
+					request.setAttribute("message", message);
 
-				RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/sogo/login.jsp?no=3");
-				dispatcher.forward(request, response);
-			}
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/sogo/login.jsp?no=3");
+					dispatcher.forward(request, response);
+				}
 
-			//IDパスワードが正しいので、IDをセッション領域に預ける
-			session.setAttribute("id",id);
+				// IDパスワードが正しいので、IDをセッション領域に預ける
+				session.setAttribute("id", id);
 
-			//ショッピングへ遷移する
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/ECHyoujiServlet");
-			dispatcher.forward(request, response);
+				if (session.getAttribute("loginid") != null) {
+					session.removeAttribute("loginid");
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/CartHyoujiServlet");
+					dispatcher.forward(request, response);
+				} else {
+					// ショッピングへ遷移する
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/ECHyoujiServlet");
+					dispatcher.forward(request, response);
 //			HttpServletResponse#sendRedirect(URL);
-
+				}
 			}
 
 		} else {
 
-			//ログアウトする
+			// ログアウトする
 			session.removeAttribute("id");
 			/* session.removeAttribute("pass"); */
 
-			//トップページへ遷移する
+			// トップページへ遷移する
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/sogo/top.jsp");
 			dispatcher.forward(request, response);
 		}
 	}
 
 }
-
-
